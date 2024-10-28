ff36e89f1685f3f11264482a915a13c9ed3822b6
Ray Chen <ray.chen@junyiacademy.org>
2024-10-28 18:30:28 +0800

    validate string and int

--- blueprints/pydantic.py
+++ blueprints/pydantic.py
@@ -1,5 +1,9 @@
+import json
+from enum import Enum
+from typing import Optional
+
 from flask import Blueprint, jsonify, request
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, Field, ValidationError
 
 pydantic_blueprint = Blueprint("pydantic_blueprint", __name__, url_prefix="/p")
 
@@ -16,5 +20,68 @@ def hello_world():
         # 驗證 JSON 資料
         user = UserModel(**request.args)
         return jsonify({"message": f"Hello {user.name}, you are {user.age} years old!"})
+    except ValidationError as e:
+        return jsonify(e.errors()), 400
+
+
+class IntModel(BaseModel):
+    default_int: int = 999
+    min_zero: int = Field(gt=0)
+    max_ten: int = Field(le=10, alias="maxTen")
+    # NOTE: 有 default value 的情況等同 allow missing
+    missing: Optional[int] = -1
+    allow_none: int | None
+
+
+# Example: `curl -d '{"min_zero":1, "maxTen":10, "allow_none":null}' -H "Content-Type: application/json" -X POST "http://127.0.0.1:5000/p/int-args"`
+@pydantic_blueprint.route("/int-args", methods=["POST"])
+def int_args_validate():
+    try:
+        # 驗證 JSON 資料
+        args = IntModel(**request.json)
+        return jsonify(
+            {
+                "default_int": args.default_int,
+                "min_zero": args.min_zero,
+                "max_ten": args.max_ten,
+                "missing": args.missing,
+                "allow_none": args.allow_none,
+            }
+        )
+
+    except ValidationError as e:
+        return jsonify(e.errors()), 400
+
+
+class UserRole(str, Enum):
+    teacher = "teacher"
+    student = "student"
+
+
+class StringModel(BaseModel):
+    allow_none: str | None
+    default_list: str = "[]"
+    max_length: str = Field(max_length=16)
+
+    user_role: UserRole = Field(alias="userRole")
+    # NOTE: 用 Enum 的做法比較漂亮，如果要堅持 str type 的話也可以用 Regex:
+    # user_role: str = Field(pattern=r"^(teacher|student)$", alias="userRole")
+
+
+# Example: `curl -d '{"allow_none":null, "userRole": "student", "max_length": "string len is 16"}' -H "Content-Type: application/json" -X POST "http://127.0.0.1:5000/p/string-args"`
+@pydantic_blueprint.route("/string-args", methods=["POST"])
+def string_args_validate():
+    try:
+        # 驗證 JSON 資料
+        args = StringModel(**request.json)
+        return jsonify(
+            {
+                "allow_none": args.allow_none,
+                "default_list": json.loads(args.default_list),
+                "user_role": args.user_role,
+                "max_length": args.max_length,
+            }
+        )
+
     except ValidationError as e:
         return jsonify(e.errors()), 400
--- blueprints/webargs.py
+++ blueprints/webargs.py
@@ -1,6 +1,8 @@
+import json
+
 from flask import Blueprint, jsonify
 from marshmallow import Schema
-from webargs import fields
+from webargs import fields, validate
 from webargs.flaskparser import use_args
 
 webargs_blueprint = Blueprint("webargs_blueprint", __name__, url_prefix="/w")
@@ -28,4 +30,56 @@ class UserSchema(Schema):
 def schema_hello_world(args):
     return jsonify(
         {"message": f"Hello {args['name']}, you are {args['age']} years old!"}
+    )
+
+
+# NOTE:
+# 1. 不太確定 missing 和 load_default 具體的差異
+int_args = {
+    "default_int": fields.Int(load_default=999),
+    "min_zero": fields.Int(required=True, validate=validate.Range(min=0)),
+    "max_ten": fields.Int(
+        data_key="maxTen", required=True, validate=validate.Range(max=10)
+    ),
+    "missing": fields.Int(missing=-1),
+    "allow_none": fields.Int(allow_none=True),
+}
+
+
+# Example: `curl -d '{"min_zero":1, "maxTen":10, "allow_none":null}' -H "Content-Type: application/json" -X POST "http://127.0.0.1:5000/w/int-args"`
+@webargs_blueprint.route("/int-args", methods=["POST"])
+@use_args(int_args)
+def int_args_validate(args):
+    return jsonify(
+        {
+            "default_int": args["default_int"],
+            "min_zero": args["min_zero"],
+            "max_ten": args["max_ten"],
+            "missing": args["missing"],
+            "allow_none": args["allow_none"],
+        }
+    )
+
+
+string_args = {
+    "allow_none": fields.Str(required=True, allow_none=True),
+    "default_list": fields.Str(required=False, load_default="[]"),
+    "user_role": fields.Str(
+        data_key="userRole", validate=validate.OneOf(["teacher", "student"])
+    ),
+    "max_length": fields.Str(required=True, validate=validate.Length(max=16)),
+}
+
+
+# Example: `curl -d '{"allow_none":null, "userRole": "student", "max_length": "string len is 16"}' -H "Content-Type: application/json" -X POST "http://127.0.0.1:5000/w/string-args"`
+@webargs_blueprint.route("/string-args", methods=["POST"])
+@use_args(string_args)
+def string_args_validate(args):
+    return jsonify(
+        {
+            "allow_none": args["allow_none"],
+            "default_list": json.loads(args["default_list"]),
+            "user_role": args["user_role"],
+            "max_length": args["max_length"],
+        }
     )
